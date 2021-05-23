095c5e9b3957f830876556f3b72cdd7884491904
heiye5 <2716634948@qq.com>
2021-05-23 21:35:58 +0800

    稀疏数组2

--- src/com/array/SparseArray.java
+++ src/com/array/SparseArray.java
@@ -64,6 +64,22 @@ public class SparseArray {
          * 1.先读取稀疏数组的第一行，根据第一行的数据，创建原始的二维数组
          * 2.再读取稀疏数组后几行的数据，并赋给二维数组即可
          */
+        int chessArr2[][] = new int[sparseArr[0][0]][sparseArr[0][1]];
+
+        //2.读取稀疏数组后几行的数据，并赋给二维数组即可
+        for(int i=1;i<sparseArr.length;i++){
+            chessArr2[sparseArr[i][0]][sparseArr[i][1]] = sparseArr[i][2];
+        }
+
+        //输出恢复后的二维数组
+        System.out.println();
+        System.out.println("恢复后的二维数组");
+        for(int [] row : chessArr2){
+            for(int data : row){
+                System.out.printf("%d\t",data);
+            }
+            System.out.println();
+        }
 
     }
 }
